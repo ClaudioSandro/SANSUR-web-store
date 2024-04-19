c63040e453387aed4cafc10f439ca38504bf853c
JHS-Sama24 <112771173+JHS-Sama24@users.noreply.github.com>
2024-04-19 09:14:44 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
-<?php include 'header.php'; ?>
+<?php include './Public/Components/header.php'; ?>
 
 <main>
     <h2>Bienvenido a Mi Sitio Web</h2>
     <p>Este es el contenido de la página de inicio.</p>
 </main>
 
-<?php include 'footer.php'; ?>
+<?php include './Public/Components/footer.php'; ?>
