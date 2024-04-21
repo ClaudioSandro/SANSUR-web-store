4fbf27f73c36591231b4fa1826c051e440c6353e
Claudio Sandro Quispesivana Torres <u202215099@upc.edu.pe>
2024-04-21 09:57:33 -0500

    comiteando php

--- index.php
+++ index.php
@@ -13,19 +13,21 @@
   <div class="opacidad" id="opacidad"></div>
   <header>
     <nav class="menu">
-      <a class="brand" href="./index.php">Sansur</a>      
+      <a class="brand" href="./index.php">Sansur</a>
       <div class="menu-items">
-        <a href="./Public/Components/index.php">TIENDA</a>
-        <a class="cajasnav" id="Registrarseid">Registrar</a>
-        <a class="cajasnav" id="iniciarid">Iniciar Sesión</a>
-        <a class="cajasnav" id="carritoid">Carrito de Compras</a>
-        <a class="icono" onclick="modooscuro()"><span id="icono">☀️</span></a>
+        <div class="search-container">
+          <input type="text" class="search-box" placeholder="Buscar...">
+          <button class="search-button">Buscar</button>
+        </div>
+          <a class="cajasnav" id="Registrarseid">Registrar</a>
+          <a class="cajasnav" id="iniciarid">Iniciar Sesión</a>
+          <a class="cajasnav" id="carritoid">Carrito de Compras</a>
       </div>
-      <div class="menuplegable">
-        <span class="bar"></span>
-        <span class="bar"></span>
-        <span class="bar"></span>
-    </div>
+        <div class="menuplegable">
+          <span class="bar"></span>
+          <span class="bar"></span>
+          <span class="bar"></span>
+        </div>
     </nav>
   </header>
   
@@ -126,37 +128,19 @@
     
     
   <section id="inicio">
-    <h1>Bag UPC</h1>
-    <p>Hola</p>
-    <p>Hola</p>
-    <a class="botoninicio">Opción C</a>
+    
   </section>
 
   <section id="valores">
-    <h2>Valores de la Startup</h2>
-    <ul>
-      <li>Productos de alta calidad</li>
-      <li>Finos accesorios</li>
-      <li>Diseños únicos</li>
-    </ul>
+    
   </section>
 
   <section id="informacion">
-    <h2>Sobre Bag UPC S.A.C.</h2>
-    <p>es una Startup peruana fundada en 2020...</p>
-    <!-- Puedes continuar con más información sobre la empresa -->
+    
   </section>
 
 
-  <section id="redes-sociales">
-    <h2>Síguenos en redes sociales</h2>
-    <ul>
-      <li><a href="https://www.facebook.com/tucuenta" target="_blank">Facebook</a></li>
-      <li><a href="https://www.instagram.com/tucuenta" target="_blank">Instagram</a></li>
-      <li><a href="https://www.linkedin.com/company/tucuenta" target="_blank">LinkedIn</a></li>
-      <li><a href="https://www.youtube.com/c/tucuenta" target="_blank">YouTube</a></li>
-    </ul>
-  </section>
+  
 
   <?php include './Public/Components/footer.php'; ?>
 
